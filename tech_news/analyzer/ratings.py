75401d32a7049a5ff8ec303ab949adf213f103fc
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-08-25 18:37:00 -0300

    feat: implementa top_five_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    print(all_news)
+    sorted_by_comments_count = sorted(
+        all_news, key=lambda k: (-k['comments_count'], k['title']))
+    list_news = sorted_by_comments_count[:5]
+
+    top_five = []
+
+    for news in list_news:
+        top_five.append((news['title'], news['url']))
+    return top_five
+    # 'comments_count'
 
 
 # Requisito 11
